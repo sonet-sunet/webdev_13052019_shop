ee6c0d21b5485f5b31e6c7610bdb50338854775a
VladimirButko <vova_butko@inbox.ru>
2019-09-11 10:13:50 +0300

    new basket

--- basket.php
+++ basket.php
@@ -23,7 +23,41 @@ if( !empty( $_SESSION['basket'] ) ){
     d($template);
 }
 ?>
-<h1>Корзина<h1>
+<div class="container">
+    <div class="container-text">
+        <h1>Ваша корзина</h1>
+        <p>Товары резервируются на ограниченное время</p>
+    </div>
+    <div class="container-box">
+        <div class="container-box-left">
+            <p>Фото</p>
+            <p>Наименование</p>
+        </div>
+        <div class="container-box-right">
+            <p>Размер</p>
+            <p>Количество</p>
+            <p>Стоимость</p>
+            <p>Удалить</p>
+        </div>
+    </div>
+    <div class="container-line"></div>
+    <div class="container-content">
+        <div class="container-content-left">
+            <div class="container-content-left-id"><?=$data_basket_item['id']?></div>
+            <div class="container-content-left-pic" style="background-image:url(<?=$data_basket_item['img1']?>)"></div>
+            <div class="container-content-left-text">
+                <p><?=$data_basket_item['name']?></p>
+                <p>арт. <?=$data_basket_item['sku']?></p>
+            </div>
+        </div>
+        <div class="container-content-right">
+            <div class="container-content-right-size"><?=$data_basket_item['size']?></div>
+            <div class="container-content-right-quantity"><?=$data_basket_item['quantity']?></div>
+            <div class="container-content-right-price"><?=$data_basket_item['full_price']?> руб.</div>
+            <div class="container-content-right-delete"></div>
+        </div>
+    </div>
+</div>
 <?php
 $footerConfig = [
     'scripts'=>['script.js', 'basket.js']
